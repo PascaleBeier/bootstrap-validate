685c3d6530b12bb9bab1191a9e35e3529b2b1da7
Colin McKenzie <cgmckenzie1@gmail.com>
2017-06-06 14:18:30 -0400

    Revert "Add regex email validation rule"
    This reverts commit f273b13de4df89cc586de6b9ced5b751e02a6e21.

--- src/rules.js
+++ src/rules.js
@@ -9,10 +9,4 @@ module.exports = {
      * @param max number
      */
   max: (input, max) => input.value.length <= max,
-	/**
-     * @param input HTMLInputElement
-     */
-  email: (input) => RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(input.value),
 };
-
-document.getElementsByTagName('input')[0].value;
