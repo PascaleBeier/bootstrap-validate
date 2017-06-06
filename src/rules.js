5243cd988c2d4c83b8a0f42ea0893614016c0e2f
Colin McKenzie <cgmckenzie1@gmail.com>
2017-06-06 12:02:59 -0700

    Add email validation rule

--- src/rules.js
+++ src/rules.js
@@ -9,8 +9,17 @@ module.exports = {
      * @param max number
      */
   max: (input, max) => input.value.length <= max,
-  	/**
+    /**
      * @param input HTMLInputElement
      */
-  email: (input) => RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(input.value),
+  email: (input) => {
+    let isValid;
+    if (!input.hasAttribute('type')) {
+      input.setAttribute('type', 'email');
+      isValid = input.valid;
+    } else {
+      isValid = RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(input.value);
+    }
+    return isValid;
+  },
 };
