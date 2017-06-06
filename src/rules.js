3924ec8327aa4cd83942fe0dc9f389c9da3ed16a
Colin McKenzie <cgmckenzie1@gmail.com>
2017-06-06 14:21:26 -0400

    Add regex email validation rule

--- src/rules.js
+++ src/rules.js
@@ -9,4 +9,8 @@ module.exports = {
      * @param max number
      */
   max: (input, max) => input.value.length <= max,
+  	/**
+     * @param input HTMLInputElement
+     */
+  email: (input) => RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(input.value),
 };
